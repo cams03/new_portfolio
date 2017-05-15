681ebfb1076117c266c951b2f8ac30d29d964100
Cams03 <camille.francia@hotmail.fr>
2017-05-15 10:24:11 +0200

    work on slideshow

--- app.js
+++ app.js
@@ -8,4 +8,23 @@ $(function () {
       window.location.hash = hash;
     });
   });
+
+  $(document).ready(function() {
+   $('.information_menu').find('li').hover(function(e) {
+      $('.information_menu').find('li').removeClass('active');
+       $(this).addClass('active');
+       $(".overlay-item").removeClass("active");
+       $(".overlay-item").removeClass("inactive");
+   $(".overlay-id"+$(this).data("id")).addClass("active").removeClass("inactive");
+
+        $(".overlay-id"+$(this).data("id")).prev().addClass("inactive")
+   });
+
+   $('.slideshow').children().on('mouseleave',function(e) {
+   $(this).removeClass("active");
+ });
+
+   $('.carousel').carousel();
+  });
+
 });
