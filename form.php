c013fc8e0411ab7e0f5cbfb37d1c1cba4c36194f
Cams03 <camille.francia@hotmail.fr>
2017-05-16 11:45:09 +0200

    working on form php

---
+++ form.php
@@ -0,0 +1,20 @@
+<?php
+$destinataire = "camille.c.francia@gmail.com";
+// Pour les champs $expediteur / $copie / $destinataire, séparer par une virgule s'il y a plusieurs adresses
+$expediteur = $_POST['email'];
+$objet = $_POST['subject'];
+$headers  = 'MIME-Version: 1.0' . "\n"; // Version MIME
+$headers .= 'Content-type: text/html; charset= UTF-8'."\n"; // l'en-tete Content-type pour le format HTML
+$headers .= 'Reply-To: '.$expediteur."\n"; // Mail de reponse
+$headers .= 'From: <'.$expediteur.'>'."\n"; // Expediteur
+$headers .= 'Delivered-to: '.$destinataire."\n"; // Destinataire
+$message = '<div style="width: 100%; text-align: center; font-weight: bold">'.$_POST['message'].'</div>';
+if (mail($destinataire, $objet, $message, $headers)) // Envoi du message
+{
+    echo 'Votre message a bien été envoyé ';
+}
+else // Non envoyé
+{
+    echo "Votre message n'a pas pu être envoyé";
+}
+?>
