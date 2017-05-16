0535fe13b7dc7c49adb6e9b995ad7aaf26d01ca6
Cams03 <camille.francia@hotmail.fr>
2017-05-16 14:52:50 +0200

    php changed

--- form.php
+++ form.php
@@ -5,10 +5,9 @@ $expediteur = $_POST['email'];
 $objet = $_POST['subject'];
 $headers  = 'MIME-Version: 1.0' . "\n"; // Version MIME
 $headers .= 'Content-type: text/html; charset= UTF-8'."\n"; // l'en-tete Content-type pour le format HTML
-$headers .= 'Reply-To: '.$expediteur."\n"; // Mail de reponse
 $headers .= 'From: <'.$expediteur.'>'."\n"; // Expediteur
 $headers .= 'Delivered-to: '.$destinataire."\n"; // Destinataire
-$message = '<div style="width: 100%; text-align: center; font-weight: bold">'.$_POST['message'].'</div>';
+$message = '<div style="width: 100%;"> De: '.$expediteur."\n".$_POST['message'].'</div>';
 if (mail($destinataire, $objet, $message, $headers)) // Envoi du message
 {
     echo 'Votre message a bien été envoyé ';
